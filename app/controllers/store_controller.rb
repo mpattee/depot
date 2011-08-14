ec6ee55d749ca4f14abd846b8c912f7e48313801
Mike Pattee <mike@cordax.net>
2011-08-14 11:25:05 -0700

    end of task D

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,11 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    if session[:counter].nil?
+      session[:counter] = 0
+    end    
+    session[:counter] += 1
+    @counter = session[:counter]
   end
 
 end
