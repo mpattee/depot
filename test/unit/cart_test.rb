7f95338f2b8bee8f5dbcbf9e439fa4051dff42f9
Mike Pattee <mike@cordax.net>
2011-08-15 09:46:33 -0700

    end E

--- test/unit/cart_test.rb
+++ test/unit/cart_test.rb
@@ -5,4 +5,20 @@ class CartTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+  
+  test "cart line item should save price" do
+    cart = carts(:one)
+    assert cart.save
+  end
+  
+  test "add two products with one line items" do
+    cart = carts(:one)
+    product = products(:ruby)
+    line = cart.add_product(product.id)
+    assert line.save
+    line = cart.add_product(product.id)
+    assert line.save
+    assert_equal 1, cart.line_items.count
+  end
+  
 end
